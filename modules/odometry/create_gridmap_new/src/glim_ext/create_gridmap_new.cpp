ee8e62c1556aff979614347f3dfbec126986557e
Masashi Izumita <m.izumita@aist.go.jp>
2024-12-17 17:47:09 +0900

    Config gridmap using json
    Introduces new gridmap configuration file
    Updates CMakeLists and source code to utilize new configuration
    Removes obsolete code and callbacks for clarity

--- modules/odometry/create_gridmap_new/src/glim_ext/create_gridmap_new.cpp
+++ modules/odometry/create_gridmap_new/src/glim_ext/create_gridmap_new.cpp
@@ -15,6 +15,7 @@
 #include <glim/util/logging.hpp>
 #include <glim/util/extension_module.hpp>
 #include <glim_ext/console_colors.hpp>
+#include <glim_ext/util/config_ext.hpp>
 
 #include <nav_msgs/msg/occupancy_grid.hpp>
 
@@ -26,6 +27,20 @@ class CreateGridmap : public ExtensionModule {
 public:
   CreateGridmap() : logger_(create_module_logger("create_gridmap")) {
     logger_ -> info("Starting create_gridmap...");
+
+    glim::Config config(glim::GlobalConfigExt::get_config_path("config_create_gridmap"));
+
+    gridmap_frame_id_ = config.param<std::string>("gridmap_param", "frame_id", "odom");
+    grid_width_ = config.param<int>("gridmap_param", "width", 60);
+    grid_height_ = config.param<int>("gridmap_param", "height", 60);
+    gridmap_origin_x_ = config.param<float>("gridmap_param", "origin_x", -25.0F);
+    gridmap_origin_y_ = config.param<float>("gridmap_param", "origin_y", -25.0F);
+    resolution_ = config.param<float>("gridmap_param", "resolution", 1.0F);
+    lower_bound_for_pt_z_ = config.param<float>("gridmap_param", "lower_bound_for_pt_z", 0.5F);
+    upper_bound_for_pt_z_ = config.param<float>("gridmap_param", "upper_bound_for_pt_z", 1.5F);
+    gridmap_data_ = std::vector<int>(grid_width_ * grid_height_, 0);
+    gridmap_realtime_data_ = std::vector<int>(grid_width_ * grid_height_, 0);
+    gridmap_submap_data_ = std::vector<int>(grid_width_ * grid_height_, 0);
 
     node_ = rclcpp::Node::make_shared("create_gridmap_new_node");
 
@@ -45,126 +60,14 @@ public:
       on_new_submap(submap);
     });
 
-    // OdometryEstimation
-    
-    OdometryEstimationCallbacks::on_new_frame.add([](const EstimationFrame::ConstPtr& frame) {
-      std::cout << console::green;
-      std::cout << boost::format("--- OdometryEstimation::on_new_frame (thread:%d) ---") %
-    std::this_thread::get_id() << std::endl; std::cout << boost::format("stamp:%.6f") % frame->stamp
-    << " frame:" << frame->id << std::endl; std::cout << console::reset;
-    });
-    OdometryEstimationCallbacks::on_update_frames.add([](const
+    OdometryEstimationCallbacks::on_update_frames.add([this](const
     std::vector<EstimationFrame::ConstPtr>& frames) { std::cout << console::green; std::cout <<
     boost::format("--- OdometryEstimation::on_update_frames (thread:%d) ---") %
     std::this_thread::get_id() << std::endl; std::cout << "frames:" << frames.size() << std::endl;
       std::cout << console::reset;
+      on_update_frames(frames);
     });
-
-    // SubMapping
-    /*
-    SubMappingCallbacks::on_insert_image.add([](const double stamp, const cv::Mat& image) {
-      std::cout << console::blue;
-      std::cout << boost::format("--- SubMapping::on_insert_image (thread:%d) ---") %
-    std::this_thread::get_id() << std::endl; std::cout << boost::format("stamp:%.6f") % stamp << "
-    image:" << image.rows << "x" << image.cols << std::endl; std::cout << console::reset;
-    });
-    SubMappingCallbacks::on_insert_imu.add([](const double stamp, const Eigen::Vector3d& a, const
-    Eigen::Vector3d& w) { std::cout << console::blue; std::cout << boost::format("---
-    SubMapping::on_insert_imu (thread:%d) ---") % std::this_thread::get_id() << std::endl; std::cout
-    << boost::format("stamp:%.6f") % stamp << " a:" << a.transpose() << " w:" << w.transpose() <<
-    std::endl; std::cout << console::reset;
-    });
-    SubMappingCallbacks::on_insert_frame.add([](const EstimationFrame::ConstPtr& frame) {
-      std::cout << console::blue;
-      std::cout << boost::format("--- SubMapping::on_insert_frame (thread:%d) ---") %
-    std::this_thread::get_id() << std::endl; std::cout << boost::format("stamp:%.6f") % frame->stamp
-    << " frame:" << frame->id << std::endl; std::cout << console::reset;
-    });
-    */
-    // SubMappingCallbacks::on_new_keyframe.add([](int id, const EstimationFrame::ConstPtr& keyframe) {
-    //   std::cout << console::blue;
-    //   std::cout << boost::format("--- SubMapping::on_new_keyframe (thread:%d) ---") %
-    //                  std::this_thread::get_id()
-    //             << std::endl;
-    //   std::cout << boost::format("id:%d") % id << " keyframe:" << keyframe->id << std::endl;
-    //   std::cout << console::reset;
-    // });
-    // SubMappingCallbacks::on_optimize_submap.add(
-    //   [](gtsam::NonlinearFactorGraph& graph, gtsam::Values& values) {
-    //     std::cout << console::blue;
-    //     std::cout << boost::format("--- SubMapping::on_optimize_submap (thread:%d) ---") %
-    //                    std::this_thread::get_id()
-    //               << std::endl;
-    //     std::cout << "factors:" << graph.size() << " values:" << values.size() << std::endl;
-    //     std::cout << console::reset;
-    //   });
-    // SubMappingCallbacks::on_optimization_status.add(
-    //   [](
-    //     const gtsam_points::LevenbergMarquardtOptimizationStatus& status,
-    //     const gtsam::Values& values) {
-    //     std::cout << console::blue;
-    //     std::cout << boost::format("--- SubMapping::on_optimization_status (thread:%d) ---") %
-    //                    std::this_thread::get_id()
-    //               << std::endl;
-    //     std::cout << status.to_string() << std::endl;
-    //     std::cout << console::reset;
-    //   });
-    // SubMappingCallbacks::on_new_submap.add([](const SubMap::ConstPtr& submap) {
-    //   std::cout << console::blue;
-    //   std::cout << boost::format("--- SubMapping::on_new_submap (thread:%d) ---") %
-    //                  std::this_thread::get_id()
-    //             << std::endl;
-    //   std::cout << "id:" << submap->id << std::endl;
-    //   std::cout << console::reset;
-    // });
-
-    // // GlobalMapping
-    // GlobalMappingCallbacks::on_insert_submap.add([](const SubMap::ConstPtr& submap) {
-    //   std::cout << console::cyan;
-    //   std::cout << boost::format("--- GlobalMapping::on_insert_submap (thread:%d) ---") %
-    //                  std::this_thread::get_id()
-    //             << std::endl;
-    //   std::cout << "submap:" << submap->id << std::endl;
-    //   std::cout << console::reset;
-    // });
-    // GlobalMappingCallbacks::on_update_submaps.add([](const std::vector<SubMap::Ptr>& submaps) {
-    //   std::cout << console::cyan;
-    //   std::cout << boost::format("--- GlobalMapping::on_update_submaps (thread:%d) ---") %
-    //                  std::this_thread::get_id()
-    //             << std::endl;
-    //   std::cout << "submaps:" << submaps.size() << std::endl;
-    //   std::cout << console::reset;
-    // });
-    // GlobalMappingCallbacks::on_smoother_update.add([](
-    //                                                  gtsam_points::ISAM2Ext& isam2,
-    //                                                  gtsam::NonlinearFactorGraph& new_factors,
-    //                                                  gtsam::Values& new_values) {
-    //   std::cout << console::cyan;
-    //   std::cout << boost::format("--- GlobalMapping::on_smoother_update (thread:%d) ---") %
-    //                  std::this_thread::get_id()
-    //             << std::endl;
-    //   std::cout << "isam2:" << isam2.size() << " new_factors:" << new_factors.size()
-    //             << " new_values:" << new_values.size() << std::endl;
-    //   std::cout << console::reset;
-    // });
-    // GlobalMappingCallbacks::on_smoother_update_result.add(
-    //   [](gtsam_points::ISAM2Ext& isam2, const gtsam_points::ISAM2ResultExt& result) {
-    //     std::cout << console::cyan;
-    //     std::cout << boost::format("--- GlobalMapping::on_smoother_update_result (thread:%d) ---") %
-    //                    std::this_thread::get_id()
-    //               << std::endl;
-    //     std::cout << result.to_string() << std::endl;
-    //     std::cout << console::reset;
-    //   });
-    // GlobalMappingCallbacks::request_to_optimize.add([]() {
-    //   std::cout << console::cyan;
-    //   std::cout << boost::format("--- GlobalMapping::request_to_optimize (thread:%d) ---") %
-    //                  std::this_thread::get_id()
-    //             << std::endl;
-    //   std::cout << console::reset;
-    // });
-  
-  } //コンストラクタ?おわり
+  }
   ~CreateGridmap() override = default;
   
   void on_new_submap(const SubMap::ConstPtr&  /*submap*/){
@@ -186,12 +89,30 @@ public:
     gridmap_pub_->publish(gridmap);
   }
 
+  void on_update_frames(const std::vector<EstimationFrame::ConstPtr>& frames) {
+    for (const auto& frame : frames) {
 
+    }
+
+  }
 
 private:
   std::shared_ptr<spdlog::logger> logger_;
   rclcpp::Node::SharedPtr node_;
   rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr gridmap_pub_;
+
+  std::vector<int> gridmap_data_;
+  std::vector<int> gridmap_realtime_data_;
+  std::vector<int> gridmap_submap_data_;
+  int grid_width_;          // gridmapの幅。gridmapのセルがいくつ横にあるか
+  int grid_height_;         // gridmapの高さ。gridmapのセルがいくつ縦にあるか
+  float gridmap_origin_x_;  // gridmapの原点[m]。ワールド座標系に対応する。
+  // 原点はGridmapの左下の点。それがワールド座標系のどこに位置するか。
+  float gridmap_origin_y_;  // gridmapの原点[m]。ワールド座標系に対応する。
+  float resolution_;        // Gridmap 1セルの高さ(幅)。1セルは正方形。
+  float lower_bound_for_pt_z_;  // Gridmapに入れる点の高さ方向のフィルタリング。最低高さ。
+  float upper_bound_for_pt_z_;  // 最高高さ。
+  std::string gridmap_frame_id_;
 };
 
 }  // namespace glim
